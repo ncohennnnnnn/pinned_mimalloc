fcf3de450ba79730685d0399e306c9d79cb04618
John Biddiscombe <biddisco@cscs.ch>
2023-11-29 19:12:24 +0100

    Rename multi-arena test and restore allocate_N one

---
+++ test/test_multi_arena.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+//
+#include <fmt/std.h>
+//
+#include <pmimalloc/allocator.hpp>
+#include "test_functions.hpp"
+
+int main()
+{
+    // minimum arena 25, maximum arena when pinning 30, maximum mmap 35
+    std::size_t mem = 1ull << 27;
+    const int nb_arenas = 5;
+    const int nb_threads = 4;
+    const int nb_allocs = 1000;
+    bool ok = true;
+
+    ok &= test_allocator_threaded_multiarena<int>(nb_arenas, nb_threads, nb_allocs, mem);
+
+    return (ok == true) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
